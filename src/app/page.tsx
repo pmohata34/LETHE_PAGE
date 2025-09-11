29f7ba19f0e3b0242dc8484ec92de301eb0b7049
Pranjal <pranjalmohata06@gmail.com>
2025-09-11 23:14:27 +0530

    Changed name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -72,7 +72,7 @@ export default function Home() {
                 height={40}
                 className="mr-3 rounded-full"
               />
-              <a href="/"><span className="italic">Go</span>Recycle</a>
+              <a href="/"><span className="italic">LETHE</a>
             </span>
             {/* Hamburger for small screens */}
             <button
